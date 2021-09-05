cf9a39b3341c4adc3ff630a4170ca36f9577d304
modulated <cowkingdom@hotmail.com>
2021-09-05 14:28:01 +1000

    add bench for reading large datafile and counting bases

---
+++ benches/seq.rs
@@ -0,0 +1,20 @@
+use criterion::*;
+use std::fs::read_to_string;
+use bio::{Seq, DNASequence};
+
+static FILENAME: &str = "benches/ecoli.fasta";
+
+fn read_counts(name: &str) {
+    let r = read_to_string(name).unwrap();
+
+    let s = Seq::new(&r);
+    s.counts();
+}
+
+
+fn run(c: &mut Criterion) {
+    c.bench_function("read_counts", |b| b.iter(||read_counts(black_box(FILENAME))));
+}
+
+criterion_group!(bench, run);
+criterion_main!(bench);
--- src/sequence/mod.rs
+++ src/sequence/mod.rs
@@ -2,10 +2,12 @@ mod dna_seq;
 mod rna_seq;
 mod protein_seq;
 pub mod fasta;
+mod orf;
 pub use dna_seq::DNASequence;
 pub use rna_seq::RNASequence;
 pub use fasta::FASTA;
 pub use protein_seq::Protein;
+pub use orf::ORF;
 
 pub trait Sequence: PartialEq {
 
@@ -13,4 +15,27 @@ pub trait Sequence: PartialEq {
     // fn reverse(&self) -> Self;
     // fn reverse_complement(&self) -> Self;
     // fn to_string(&self) -> String;
+}
+
+pub struct Seq(String);
+
+impl Seq {
+    pub fn new(string: &str) -> Self {
+        Seq(string.to_owned())
+    }
+
+    pub fn counts(&self) -> (u32, u32, u32, u32) {
+        let mut out = (0,0,0,0);
+        for c in self.0.chars() {
+            match c {
+                'A' | 'a' => out.0 += 1,
+                'C' | 'c' => out.1 += 1,
+                'G' | 'g' => out.2 += 1,
+                'T' | 't' => out.3 += 1,
+                _ => {}
+            }
+        }
+
+        out
+    }
 }
