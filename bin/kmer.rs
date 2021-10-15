7d059669eb1a4ba2f6ea57643fe2ea0da24e6d76
George Roubos <cowkingdom@hotmail.com>
2021-10-15 22:26:46 +1100

    add kmer bin

---
+++ bin/kmer.rs
@@ -0,0 +1,21 @@
+use std::env::args;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+	println!("KMER Problem");
+	if args().len() < 2 {
+		println!("Please supply file as argument.");
+		return Ok(());
+	}
+
+	let filestring = std::fs::read_to_string(args().nth(1).ok_or("File not found")?)?;
+	let fasta = &bio::parse_string_to_vec_of_fasta(&filestring)[0];
+
+    let r = bio::combinatorics::kmer::lexographical_kmer_composition(fasta.seq.to_slice(), &[b'A',b'C',b'G',b'T'][..], 4);
+
+    for x in r {
+        print!("{} ", x);
+    }
+    println!();
+
+	Ok(())
+}
