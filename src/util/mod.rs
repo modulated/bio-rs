7f81b1f76e0bf7358058d9446b323b2db14626b6
George Roubos <cowkingdom@hotmail.com>
2021-10-18 18:26:49 +1100

    mortal fib rabbit function

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -18,3 +18,50 @@ pub fn fibonacci_rabbits(
 		}
 	}
 }
+
+pub fn mortal_fibonacci_rabbits(
+	months: i64,
+	lifespan: i64,
+	memo: &mut std::collections::HashMap<i64, i64>,
+) -> i64 {	
+	if memo.contains_key(&months) {
+		return memo[&months];
+	}
+
+	let out = match months {
+		x if x < 0 => 0,
+		0 => 0,
+		1 => 1,		
+		_ => {
+			if months <= lifespan {
+				mortal_fibonacci_rabbits(months - 1, lifespan, memo)
+					+ mortal_fibonacci_rabbits(months - 2, lifespan, memo)
+			}
+			else if months == lifespan + 1 {
+				mortal_fibonacci_rabbits(months - 1, lifespan, memo)
+					+ mortal_fibonacci_rabbits(months - 2, lifespan, memo)
+					- 1
+			}
+			else {
+				mortal_fibonacci_rabbits(months - 1, lifespan, memo)
+					+ mortal_fibonacci_rabbits(months - 2, lifespan, memo)
+					- mortal_fibonacci_rabbits(months - (lifespan + 1), lifespan, memo)
+			}
+		}
+	};
+
+	memo.insert(months, out);
+	out
+}
+
+#[cfg(test)]
+mod test {
+    use crate::util::mortal_fibonacci_rabbits;
+	
+	#[test]
+	fn test_mortal() {
+		let mut map = std::collections::HashMap::new();
+		assert_eq!(mortal_fibonacci_rabbits(6, 3, &mut map), 4);
+		println!("{:?}", map);
+	}
+}
