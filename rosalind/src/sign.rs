30237cd7f0a5ab50bdb6b064fe2fac9c5ca78683
George Roubos <cowkingdom@hotmail.com>
2021-10-26 11:04:32 +1100

    solved sign problem

---
+++ rosalind/src/sign.rs
@@ -0,0 +1,21 @@
+use std::env::args;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+	println!("SIGN Problem");
+	if args().len() != 2 {
+		println!("Please supply num as argument.");
+		return Ok(());
+	}
+
+	let intg = args().nth(1).ok_or("File not found.")?;
+	let input: u8 = intg.parse()?;
+
+	let out = bio::combinatorics::permutation::signed_permuatations(input);
+
+    println!("{}", out.len());
+    for l in out {
+        println!("{}", bio::util::slice_to_string(&l));
+    }
+
+	Ok(())
+}
