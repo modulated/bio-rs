b944bbfb59b468f3dc389edef311d2944cd27ac9
modulated <cowkingdom@hotmail.com>
2021-10-29 02:08:08 +1100

    phred reading refactor fastq to vec

--- src/formats/fastq.rs
+++ src/formats/fastq.rs
@@ -1,35 +1,47 @@
-use crate::Seq;
+use crate::{BioResult, Seq};
 use std::fmt::Display;
 
 #[derive(PartialEq, Debug)]
 pub struct FASTQ {
 	pub name: String,
 	pub seq: Seq,
-	pub qual: Seq,
+	pub qual: Vec<u8>,
 }
 
 impl FASTQ {
 	#[must_use]
 	pub fn new(string: &str) -> Self {
-		let (name, data) = string
-			.trim()
-			.trim_start_matches('@')
-			.split_once('\n')
-			.unwrap();
+		let string: Vec<&str> = string.split('\n').collect();
 
-		let (seq, qual) = data.trim().split_once('+').unwrap();
-
-		Self {
-			name: name.to_string(),
-			seq: Seq::new(seq),
-			qual: Seq::new(qual),
-		}
+		let out = Self {
+			name: string[0].trim_start_matches('@').to_string(),
+			seq: Seq::new(string[1].trim()),
+			qual: string[3].trim().as_bytes().to_vec(),
+		};
+		
+		out
 	}
 
 	#[must_use]
 	pub fn from_file(file: &str) -> Self {
 		Self::new(&std::fs::read_to_string(file).unwrap())
 	}
+
+	#[must_use]
+	pub fn phred_quality(&self) -> Vec<u8> {
+		self.qual.iter().map(|x|phred_to_int(*x)).collect()
+	}
+
+	pub fn average_quality(&self) -> BioResult<f64> {
+		use std::convert::TryFrom;
+		let num: f64 = self.phred_quality().iter().map(|x|u32::from(*x)).sum::<u32>().into();
+		let denom: f64 = u32::try_from(self.qual.len())?.into();
+		Ok(num / denom)
+	}
+}
+
+const fn phred_to_int(ph: u8) -> u8 {
+	ph - b'!'
 }
 
 impl Display for FASTQ {
@@ -53,18 +65,43 @@ mod test {
 	fn new() {
 		let f = FASTQ::new(
 			r"@HWI-ST999:102:D1N6AACXX:1:1101:1235:1936 1:N:0:
-ATGTCTCCTGGACCCCTCTGTGCCCAAGCTCCTCATGCATCCTCCTCAGCAACTTGTCCTGTAGCTGAGGCTCACTGACTACCAGCTGCAG
-+
-1:DAADDDF\<B\<AGF=FGIEHCCD9DG=1E9?D>CF@HHG??B\<GEBGHCG;;CDB8==C@@>>GII@@5?A?@B>CEDCFCC:;?CCCAC
-",
+			ATGTCTCCTGGACCCCTCTGTGCCCAAGCTCCTCATGCATCCTCCTCAGCAACTTGTCCTGTAGCTGAGGCTCACTGACTACCAGCTGCAG
+			+
+			1:DAADDDF\<B\<AGF=FGIEHCCD9DG=1E9?D>CF@HHG??B\<GEBGHCG;;CDB8==C@@>>GII@@5?A?@B>CEDCFCC:;?CCCAC
+			",
 		);
 		assert_eq!(f.name, "HWI-ST999:102:D1N6AACXX:1:1101:1235:1936 1:N:0:");
 		assert_eq!(f.seq, Seq::new("ATGTCTCCTGGACCCCTCTGTGCCCAAGCTCCTCATGCATCCTCCTCAGCAACTTGTCCTGTAGCTGAGGCTCACTGACTACCAGCTGCAG"));
 		assert_eq!(
 			f.qual,
-			Seq::new(
-				r"1:DAADDDF\<B\<AGF=FGIEHCCD9DG=1E9?D>CF@HHG??B\<GEBGHCG;;CDB8==C@@>>GII@@5?A?@B>CEDCFCC:;?CCCAC"
-			)
+			br#"1:DAADDDF\<B\<AGF=FGIEHCCD9DG=1E9?D>CF@HHG??B\<GEBGHCG;;CDB8==C@@>>GII@@5?A?@B>CEDCFCC:;?CCCAC"#
 		);
+	}
+
+	#[test]
+	fn phred_quality() {
+		let f = FASTQ::new(r#"@Rosalind_0041
+			GGCCGGTCTATTTACGTTCTCACCCGACGTGACGTACGGTCC
+			+
+			6.3536354;.151<211/0?::6/-2051)-*"40/.,+%))"#);
+		let output = vec![21, 13, 18, 20, 18, 21, 18, 20, 19, 26, 13, 16, 20, 16, 27, 17, 16, 16, 14, 15, 30, 25, 25, 21, 14, 12, 17, 15, 20, 16, 8, 12, 9, 1, 19, 15, 14, 13, 11, 10, 4, 8, 8];
+		
+		assert_eq!(f.phred_quality(), output);
+	}
+
+	#[test]
+	fn average_quality() {
+		let f = FASTQ::new(r#"@Rosalind_0041
+			GGCCGGTCTATTTACGTTCTCACCCGACGTGACGTACGGTCC
+			+
+			6.3536354;.151<211/0?::6/-2051)-*"40/.,+%))"#);
+		assert_eq!(format!("{:.4}", f.average_quality().unwrap()), 16.0698.to_string());
+	}
+
+	#[test]
+	fn phred_convert() {
+		use super::phred_to_int as phr;
+		assert_eq!(phr(b'!'), 0);
+		assert_eq!(phr(b'A'), 32);
 	}
 }
