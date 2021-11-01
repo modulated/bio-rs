98b2982807b87b748f06016321500f756d0bedee
George Roubos <cowkingdom@hotmail.com>
2021-11-01 17:59:20 +1100

    remove large benches - generates large folder size

--- benches/seq.rs
+++ benches/seq.rs
@@ -1,37 +1,26 @@
 use bio::FASTA;
 use criterion::*;
 
-static ECOLI: &str = "benches/ecoli.fasta";
 static SAL: &str = "benches/salmonella.fasta";
 
-fn read_counts(name: &str) {
-	let r = FASTA::from_file(name);
-
-	let _ = r.seq.counts();
+fn read_counts(f: &FASTA) {
+	let _r = f.seq.counts();
 }
 
-fn orf(fasta: &FASTA) {
-	let _r = fasta.seq.orf(1);
+fn orf(f: &FASTA) {
+	let _r = f.seq.orf(1);
 }
 
-fn len(seq: &FASTA) {
-	let _r = seq.seq.len();
+fn len(f: &FASTA) {
+	let _r = f.seq.len();
 }
 
 fn run(c: &mut Criterion) {
-	c.bench_function("read_counts", |b| b.iter(|| read_counts(black_box(ECOLI))));
-
 	let sal = FASTA::from_file(SAL);
 
-	c.bench_with_input(BenchmarkId::new("ORF Salmonella", &sal), &sal, |b, s| {
-		b.iter(|| orf(s));
-	});
-
-	let ecoli = FASTA::from_file(ECOLI);
-
-	c.bench_with_input(BenchmarkId::new("Len E Coli", &ecoli), &ecoli, |b, s| {
-		b.iter(|| len(s));
-	});
+	c.bench_function("LEN", |b| b.iter(|| len(&sal)));
+	c.bench_function("COUNTS", |b| b.iter(|| read_counts(&sal)));
+	c.bench_function("ORF", |b| b.iter(|| orf(&sal)));
 }
 
 criterion_group!(bench, run);
